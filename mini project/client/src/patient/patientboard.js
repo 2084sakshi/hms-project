28121cfc91754a52fe25fe566ca004835c5865ac
SRUSHTI PAWAR <142104311+srush25@users.noreply.github.com>
2024-02-15 19:02:00 +0530

    Update patientboard.js

--- mini project/client/src/patient/patientboard.js
+++ mini project/client/src/patient/patientboard.js
@@ -1,23 +1,33 @@
 // PatientDashboard.js
 import React from 'react';
 import PatientHeader from './patientheader';
-
+import './../css/patient.css';
+import Book from './../img/Book.svg';
+import MediHist from './../img/medicalhist.svg';
+import viewappt from './../img/viewappt.svg';
+import profile from './../img/profile.svg';
+import settings from './../img/settings.svg';
 function PatientDashboard() {
   return (
     <div>
       <PatientHeader />
-      <div>
-        <h2>Welcome to Your Dashboard</h2>
+      <div className='patientheader'>
+        <h2>Welcome to Your Dashboard!</h2>
         <p>
           Thank you for choosing Medconnect360 for your healthcare needs. This dashboard provides you with
           quick access to essential features. Here's what you can do:
         </p>
-        <ul>
+        <ul className='dash-links'>
+          <img src={Book} style={{width:200 ,innerHeight:100}}/>
           <li><strong>Book Appointment:</strong> Schedule your medical appointments with ease.</li>
+          <img src={viewappt} style={{width:200 ,innerHeight:100}}/>
           <li><strong>View Appointments:</strong> Check your upcoming and past appointments.</li>
+          <img src={MediHist} style={{width:200 ,innerHeight:100}}/>
           <li><strong>Medical History:</strong> Review your medical history and diagnoses.</li>
+          <img src={profile} style={{width:200 ,innerHeight:100}}/>
           <li><strong>Profile:</strong> Update your personal information and preferences.</li>
-          <li><strong>Settings:</strong> Customize your account settings and notification preferences.</li>
+          <img src={settings} style={{width:200 ,innerHeight:100}}/>
+          <li><strong> Dynamic Settings:</strong> Customize your account settings and notification preferences.</li>
         </ul>
         <p>
           Our dedicated team of healthcare professionals is here to ensure you receive the best care.
