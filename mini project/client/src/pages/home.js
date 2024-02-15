a478123a7745ec8db7e552d3e971b7480d2f07cf
SRUSHTI PAWAR <142104311+srush25@users.noreply.github.com>
2024-02-15 18:58:22 +0530

    Update home.js

--- mini project/client/src/pages/home.js
+++ mini project/client/src/pages/home.js
@@ -1,6 +1,12 @@
 // LandingPage.js
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './../css/home.css';
+import './../css/admin.css';
+import logo from './../img/logo.svg';
+import Homepage from './../img/homepage1.svg';
+import doctor from './../img/doctor.svg';
+import patient from './../img/patient.svg';
 
 function Home() {
   const [showLoginRegister, setShowLoginRegister] = useState(null);
@@ -12,93 +18,156 @@ function Home() {
   return (
     <div className="landing-page">
       {/* Header Section */}
-      <header>
-        <div className="logo">
-          <img src="/path/to/logo.png" alt="Medconnect360 Logo" />
+      <header className="home-header">
+        <div className="logo-container">
+          <img src={logo} alt="MedConnect360 logo"/>
           <h1>Medconnect360</h1>
         </div>
-        {/* 
-        <nav>
-          <ul>
-            <li>
-              <Link to="/admin">Admin</Link>
+
+        <nav className="quick-links">
+          <ul className="nav-links">
+            {/*<li>
+            <div
+                className="dropdown-trigger"
+                onClick={() => handleDropdownClick('admin')}
+              >
+                Admin    
+          
               {showLoginRegister === 'admin' && (
                 <div className="dropdown">
                   <Link to="/login">Login</Link>
                   <Link to="/register">Register</Link>
                 </div>
               )}
+              </div>
             </li>
-            <li>
-              <Link to="/doctor">Doctor</Link>
+              */}
+               <li className='nav-links'><a href = "#home">Home</a> </li>
+               <li className='nav-links'><a href = "#about">About</a>
+            </li>
+            <li className='nav-links'> <a href = "#contact">Contact</a> </li>
+            <li className='nav-links'>
+            <div
+                className="dropdown-trigger"
+                onClick={() => handleDropdownClick('doctor')}
+              >
+                Doctor
               {showLoginRegister === 'doctor' && (
                 <div className="dropdown">
-                  <Link to="/login">Login</Link>
-                  <Link to="/register">Register</Link>
+                  <Link to="/login-doctor">Login</Link>
+                  <Link to="/register-doctor">Register</Link>
                 </div>
               )}
+              </div>
             </li>
-            <li>
-              <Link to="/patient">Patient</Link>
+            <li className='nav-links'>
+
+              <div
+                className="dropdown-trigger"
+                onClick={() => handleDropdownClick('patient')}
+              >
+                Patient
               {showLoginRegister === 'patient' && (
                 <div className="dropdown">
-                  <Link to="/login">Login</Link>
-                  <Link to="/register">Register</Link>
+                  <Link to="/login-patient">Login</Link>
+                  <Link to="/register-patient">Register</Link>
                 </div>
               )}
-            </li>
+              </div>
+            </li>
+           
           </ul>
         </nav>
-        */}
-        <nav >
-        <ul>
-          <li>
-        <Link to="/login">Login</Link>
-        </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          </ul>
-          </nav>
 
+        
+        { /*<nav className='quick-links'>
+      <ul>
+        <li>
+          <div className="dropdown">
+            <button className="dropbtn">Login</button>
+            <div className="dropdown-content">
+              <Link to="/login?type=patient">Patient</Link>
+              <Link to="/login?type=doctor">Doctor</Link>
+              <Link to="/login?type=admin">Admin</Link>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className="dropdown">
+            <button className="dropbtn">Register</button>
+            <div className="dropdown-content">
+              <Link to="/register?type=patient">Patient</Link>
+              <Link to="/register?type=doctor">Doctor</Link>
+              <Link to="/register?type=admin">Admin</Link>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </nav>
+*/}
           
       </header>
 
       {/* Welcome Section */}
       <section className="welcome-section">
-        <h2>Embrace Your Health Journey with Medconnect360</h2>
+        <div className='top-section'>
+        <h2>Embrace Your Health Journey with <span>Medconnect360</span></h2>
         <p>
           Welcome to Medconnect360, where health meets technology in a seamless dance. Schedule and manage appointments effortlessly for both doctors and patients. Let's embark on a journey to prioritize your well-being.
         </p>
+  
+        </div>
+        
       </section>
 
+
       {/* Features Section */}
-      <section className="features-section">
+      <section id = "about">
+             <section className="features-section">
         <div className="feature">
-          <h3>For Doctors</h3>
-          <ul>
+         <div className='comparison-item'>
+          <img src={doctor} alt='doctor'/>
+          <mark className="text-branch"><h3>For Doctors</h3></mark>
+           <ul>
             <li>Effortlessly orchestrate appointments</li>
             <li>Dive into patient histories with grace</li>
             <li>Maintain your availability like a symphony</li>
-          </ul>
+           </ul>
+          </div>
         </div>
         <div className="feature">
-          <h3>For Patients</h3>
+        <div className='comparison-item'>
+        <img src={patient} alt='patient'/>
+        <mark className="text-branch1"><h3>For Patients</h3></mark>
           <ul>
-            <li>Schedule appointments with the finesse of a virtuoso</li>
+            <li >Schedule appointments with the finesse of a virtuoso</li>
             <li>Access medical records at your fingertips</li>
             <li>Stay harmoniously informed about your health journey</li>
           </ul>
-        </div>
-        <div className="feature">
-          <h3>Admin Dashboard</h3>
-          <ul>
-            <li>Streamline user management with the precision of a conductor</li>
-            <li>Monitor appointments in a rhythm that suits you</li>
-            <li>Analyze system analytics for a symphony of insights</li>
-          </ul>
+          </div>
         </div>
       </section>
+      </section>
+      <div className='contact-us'>
+      <section id="contact" class = "contact">
+        <div class = "title"  data-aos="fade-down" >
+          <div className='card'>
+            <h2>Contact <span>Us</span></h2>
+            </div>
+        </div>
+        <div class = "contact-form"  data-aos="zoom-in" >
+        <div className='card'>
+            <form action = "">
+                <input type="text" placeholder="Enter Name" class="box" required></input>
+                <input type="email" placeholder="Enter Email" class="box" required></input>
+                <textarea name="" id="" cols="30" rows="10" placeholder="Enter Message" class="box1"></textarea>
+                <input type="submit" value="Send" class="btn-submit"></input>
+            </form>
+            </div>
+        </div>
+    
+      </section>
+      </div>
     </div>
   );
 }
